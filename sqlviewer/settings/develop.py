707c5cc8a174a22116222cd7b9278aaa2d7f5bef
Stefan Martinov <stefan.martinov@gmail.com>
2016-02-27 10:43:37 +0100

    Reverted to sqlite3 default database

--- sqlviewer/settings/develop.py
+++ sqlviewer/settings/develop.py
@@ -8,11 +8,7 @@ DEBUG = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'glimpse',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',  # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
     }
 }
